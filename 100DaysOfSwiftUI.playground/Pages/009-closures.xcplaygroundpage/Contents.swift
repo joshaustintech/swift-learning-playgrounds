6985aec63016b223625f5832870bc81ad0abbc80
Joshua Austin <83935880+joshaustintech@users.noreply.github.com>
2023-01-10 02:07:08 -0600

    closures

---
+++ 100DaysOfSwiftUI.playground/Pages/009-closures.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+let sayHello = { (name: String) -> String in
+    "Hi \(name)!"
+}
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+
+let captainFirstTeam = team.sorted {
+    if $0 == "Suzanne" {
+        return true
+    } else if $1 == "Suzanne" {
+        return false
+    }
+
+    return $0 < $1
+}
+
+let reverseTeam = team.sorted { $0 > $1 }
+
+let tOnly = team.filter { $0.hasPrefix("T") }
+print(tOnly)
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
+
+func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start first work")
+    first()
+    print("About to start second work")
+    second()
+    print("About to start third work")
+    third()
+    print("Done!")
+}
+
+doImportantWork {
+    print("This is the first work")
+} second: {
+    print("This is the second work")
+} third: {
+    print("This is the third work")
+}
